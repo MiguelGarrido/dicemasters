b64686b9d884e502aa873728833d66d4d272aaf6
Miguel Garrido Miranda <magamimi@gmail.com>
2014-06-03 15:03:57 +0200

    CardImage added to Character

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,7 @@ Dicemasters::Application.configure do
 
   # AWS S3
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_host_name => 's3.amazonaws.com',
-    :s3_protocol => 'https',
-    :s3_permissions => :private,
+    :storage => :s3,    
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
